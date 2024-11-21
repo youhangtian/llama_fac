e9cd42e39352d83691fdf51fa2248d664897e7f8
tian <1013515028@qq.com>
2024-11-21 11:26:36 +0800

    add qwenvl

--- sft.sh
+++ sft.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=2,3,4,5
 export PYTHONPATH=$PYTHONPATH:/home/tyh/git/LLaMA-Factory/src
 
-python -c 'from llamafactory.cli import main; main()' train ./config/llava_lora_sft.yaml
-python -c 'from llamafactory.cli import main; main()' export ./config/llava_lora_merge.yaml
+python -c 'from llamafactory.cli import main; main()' train ./config/vlm_lora_sft.yaml
+python -c 'from llamafactory.cli import main; main()' export ./config/vlm_lora_merge.yaml
